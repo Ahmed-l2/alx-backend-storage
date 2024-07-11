7e4f6c718c13fca73216304faefd61dd3bae5d50
Ahmed <ahmed_l2@yahoo.com>
2024-07-11 23:28:19 +0300

    7. Average score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -8,8 +8,8 @@ BEGIN
     DECLARE avg_score FLOAT;
     
     SELECT AVG(score) INTO avg_score
-    FROM corrections
-    WHERE user_id = user_id;
+    FROM corrections c
+    WHERE c.user_id = user_id;
     
     UPDATE users
     SET average_score = avg_score
