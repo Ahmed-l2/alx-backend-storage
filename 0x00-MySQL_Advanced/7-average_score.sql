f4240340d9164eb217307d032ebfc22ac6581738
Ahmed <ahmed_l2@yahoo.com>
2024-07-11 23:19:08 +0300

    7. Average score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -5,15 +5,14 @@
 DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-  DECLARE avg_score FLOAT;
-  
-  SELECT AVG(score) INTO avg_score
+    DECLARE avg_score FLOAT;
+    
+    SELECT AVG(score) INTO avg_score
     FROM corrections
     WHERE user_id = user_id;
     
     UPDATE users
     SET average_score = avg_score
     WHERE id = user_id;
-  END //
-
+END //
 DELIMITER ;
