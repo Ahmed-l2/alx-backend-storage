6cf76d4e50cd7e8f4fed316193635250f7a6c784
Ahmed <ahmed_l2@yahoo.com>
2024-07-16 22:16:40 +0300

    12. Log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,25 +1,30 @@
 #!/usr/bin/env python3
 import pymongo
-"""Provides some stats about Nginx logs stored in MongoDB"""
+"""Module for log_stats"""
 
 
+def log_stats():
+    """Provides some stats about Nginx logs stored in MongoDB"""
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    logs = myclient["logs"]
+    nginx = logs["nginx"]
 
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-logs = myclient["logs"]
-nginx = logs["nginx"]
+    GET = len(list(nginx.find({"method": "GET"})))
+    POST = len(list(nginx.find({"method": "POST"})))
+    PUT = len(list(nginx.find({"method": "PUT"})))
+    PATCH = len(list(nginx.find({"method": "PATCH"})))
+    DELETE = len(list(nginx.find({"method": "DELETE"})))
+    status_check = len(list(nginx.find({"method": "GET", "path": "/status"})))
 
-GET = len(list(nginx.find({"method": "GET"})))
-POST = len(list(nginx.find({"method": "POST"})))
-PUT = len(list(nginx.find({"method": "PUT"})))
-PATCH = len(list(nginx.find({"method": "PATCH"})))
-DELETE = len(list(nginx.find({"method": "DELETE"})))
-status_check = len(list(nginx.find({"method": "GET", "path": "/status"})))
+    print(f"{len(list(nginx.find()))} logs")
+    print("Methods:")
+    print(f"\t method GET: {GET}")
+    print(f"\t method POST: {POST}")
+    print(f"\t method PUT: {PUT}")
+    print(f"\t method PATCH: {PATCH}")
+    print(f"\t method DELETE: {DELETE}")
+    print(f"{status_check} status check")
 
-print(f"{len(list(nginx.find()))} logs")
-print("Methods:")
-print(f"\t method GET: {GET}")
-print(f"\t method POST: {POST}")
-print(f"\t method PUT: {PUT}")
-print(f"\t method PATCH: {PATCH}")
-print(f"\t method DELETE: {DELETE}")
-print(f"{status_check} status check")
+
+if __name__ == "__main__":
+    log_stats()
