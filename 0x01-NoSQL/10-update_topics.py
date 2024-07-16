7586d9bf4516d43de7dcbf9f6d6f96784dd7e6ea
Ahmed <ahmed_l2@yahoo.com>
2024-07-16 17:19:25 +0300

    10. Change school topics

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -6,6 +6,6 @@ def update_topics(mongo_collection, name, topics):
     """changes all topics of a school document based on the name"""
 
     filter = {"name": name}
-    update = {"set": {"topics": topics}}
+    update = {"$set": {"topics": topics}}
 
     mongo_collection.update_many(filter, update)
