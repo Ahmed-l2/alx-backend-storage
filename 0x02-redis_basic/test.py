f5e5fd466c7538c002b02bcab3e41e2f80a2e76c
Ahmed <ahmed_l2@yahoo.com>
2024-07-18 01:18:01 +0300

    5. Implementing an expiring web cache and tracker

--- 0x02-redis_basic/test.py
+++ 0x02-redis_basic/test.py
@@ -1,27 +1,7 @@
 #!/usr/bin/env python3
-"""
-Main file
-"""
-import redis
 
-Cache = __import__('exercise').Cache
+get_page = __import__('web').get_page
 
-cache = Cache()
+result = get_page('https://google.com')
 
-# data = b"hello"
-# key = cache.store(data)
-# print(key)
-
-# local_redis = redis.Redis()
-# print(local_redis.get(key))
-
-TEST_CASES = {
-    b"foo": None,
-    123: int,
-    "bar": lambda d: d.decode("utf-8")
-}
-
-for value, fn in TEST_CASES.items():
-    key = cache.store(value)
-    assert cache.get(key, fn=fn) == value
-    print(cache.get(key, fn=fn))
+print(result)
---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Module for get_page function"""
+import requests
+from functools import wraps
+import redis
+
+r = redis.Redis()
+
+
+def url_access_count(method):
+    """Decorator to count how many times a URL has been accessed"""
+    @wraps(method)
+    def wrapper(url):
+        cached_key = "cached:{}".format(url)
+        cached_html = r.get(cached_key)
+        if cached_html:
+            return cached_html.decode("utf-8")
+
+        count_key = "count:{}".format(url)
+        r.incr(count_key)
+
+        html = method(url)
+        r.setex(cached_key, 10, html)
+        return html
+    return wrapper
+
+
+def get_page(url: str) -> str:
+    """Obtains the HTML content of a particular URL and returns it."""
+    response = requests.get(url)
+    return response.text
+
+
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
